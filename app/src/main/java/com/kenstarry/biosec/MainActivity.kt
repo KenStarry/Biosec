a98f00e2d02546e39c8e24ba6f711e22f4ec520b
KenStarry <starrycodes@gmail.com>
2023-05-07 21:46:12 +0300

    Biosec Splash Screen

--- app/src/main/java/com/kenstarry/biosec/MainActivity.kt
+++ app/src/main/java/com/kenstarry/biosec/MainActivity.kt
@@ -17,12 +17,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kenstarry.biosec.ui.custom.spacing
 import com.kenstarry.biosec.ui.theme.BiosecTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             BiosecTheme {
 
