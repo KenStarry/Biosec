debae4cfd05601f34771b6105a96490b2e160641
KenStarry <starrycodes@gmail.com>
2023-05-07 18:31:19 +0300

    Added custom Spacing to Material Theme

--- app/src/main/java/com/kenstarry/biosec/MainActivity.kt
+++ app/src/main/java/com/kenstarry/biosec/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.kenstarry.biosec
 
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.content.res.Configuration.UI_MODE_TYPE_WATCH
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kenstarry.biosec.ui.custom.spacing
 import com.kenstarry.biosec.ui.theme.BiosecTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/kenstarry/biosec/ui/theme/Theme.kt
+++ app/src/main/java/com/kenstarry/biosec/ui/theme/Theme.kt
@@ -9,11 +9,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.kenstarry.biosec.ui.custom.LocalSpacing
+import com.kenstarry.biosec.ui.custom.Spacing
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -62,9 +65,13 @@ fun BiosecTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing()
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
