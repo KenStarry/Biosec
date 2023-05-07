05fc7a24fc671f8ea99c9f031062c25e0b137a81
KenStarry <starrycodes@gmail.com>
2023-05-08 00:34:24 +0300

    Added various screen routes

---
+++ app/src/main/java/com/kenstarry/biosec/feature_generator/presentation/GeneratorScreen.kt
@@ -0,0 +1,9 @@
+package com.kenstarry.biosec.feature_generator.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GeneratorScreen() {
+    Text(text = "Generator Screen")
+}
---
+++ app/src/main/java/com/kenstarry/biosec/navigation/NavConstants.kt
@@ -0,0 +1,18 @@
+package com.kenstarry.biosec.navigation
+
+object NavConstants {
+
+    const val ROOT_ROUTE = "root"
+    const val AUTH_ROUTE = "authentication"
+    const val HOME_ROUTE = "authentication"
+
+    //  screen routes
+    const val MAIN_SCREEN_ROUTE = "main screen route"
+    const val AUTH_SCREEN_ROUTE = "auth screen route"
+
+    //  bottom nav screens
+    const val BOTTOM_HOME_ROUTE = "bottom nav screen home"
+    const val BOTTOM_GENERATOR_ROUTE = "bottom nav screen generator"
+    const val BOTTOM_STATS_ROUTE = "bottom nav screen stats"
+    const val BOTTOM_SETTINGS_ROUTE = "bottom nav screen settings"
+}
